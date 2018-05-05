196a0fec6d9521677fc2065c6742a8057d27d2e8
Tejareddy Duggirala <duggirala.tejareddy@gmail.com>
2018-05-05 17:49:08 -0400

    Angular tutorial
    Angular tutorial
    
    Co-Authored-By: duggi8 <duggirala.tejareddy@gmail.com>

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import {Hero} from './hero';
+
+export const HEROES:Hero[]=[
+  {id:11,name:'Mr.Terrific'},
+  {id:12,name:'Flash'},
+  {id:13,name:'Black Canary'},
+  {id:14,name:'Spartan'},
+  {id:15,name:'Overwatch'},
+  {id:16,name:'The Hood'},
+  {id:17,name:'Deadshot'},
+  {id:18,name:'Green Arrow'}
+];
